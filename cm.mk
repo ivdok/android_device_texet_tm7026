ce8d856ea261a14de6e2f5493acef4994008057a
ivdok <ivdok@yandex.ru>
2013-04-06 18:38:43 +0400

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,21 +1,21 @@
 # Release name
-PRODUCT_RELEASE_NAME := MID08
+PRODUCT_RELEASE_NAME := NUCLEAR-EVB
 
 # Boot animation
-TARGET_BOOTANIMATION_NAME := vertical-480x800
+TARGET_BOOTANIMATION_NAME := horizontal-480x800
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/manta/mid08/full_mid08.mk)
+$(call inherit-product, device/texet/nuclear-evb/full_nuclear-evb.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := mid08
-PRODUCT_NAME := cm_mid08
-PRODUCT_BRAND := Manta
-PRODUCT_MODEL := MID08
-PRODUCT_MANUFACTURER := Manta
+PRODUCT_DEVICE := nuclear-evb
+PRODUCT_NAME := cm_nuclear-evb
+PRODUCT_BRAND := texet
+PRODUCT_MODEL := nuclear-evb
+PRODUCT_MANUFACTURER := texet
 
 #Set build fingerprint / ID / Product Name etc
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mid08 BUILD_FINGERPRINT="Manta/mid08/mid08:4.0.4/IMM76I/330937:user/release-keys" PRIVATE_BUILD_DESC="mid08-user 4.0.4 IMM76I 330937 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nuclear-evb BUILD_FINGERPRINT="/texet/nuclear-evb/nuclear-evb:4.0.4/IMM76I/330937:user/release-keys" PRIVATE_BUILD_DESC="nuclear-evb-user 4.0.4 IMM76I 330937 release-keys"
